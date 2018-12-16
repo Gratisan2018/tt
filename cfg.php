fe46417442bf4c5f561beafcd35780bdd4879824
xgrime <45916886+xgrime@users.noreply.github.com>
2018-12-16 23:35:09 +0700

    Create cfg.php

---
+++ cfg.php
@@ -0,0 +1,10 @@
+<?php
+//ikuti format config di bawah "ticket=tiket_kamu&meid=meid_kamu",
+//jangan ada sepasi
+$config = [
+
+"ticket=sHp2r312otmCpnGns3mCqJhovNiBh3jPr7qja7OTitm8sIpofay2k4SmlJi_Y4Kbhn2szX53hM264JKks5N_y62jra6KZM3XjZSKo7-Ik2ubfrCUi4RzoQ&meid=aa12d572eba63cd9",
+
+//ganti dengan ticket dan meid kalian
+];
+?>
